215be49ae1a30290935d0f2290e7f86a38ae32d0
Eighty6 <aaagent86@gmail.com>
2017-07-30 13:26:30 -0700

    Initial commit

---
+++ DNDBot/Program.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace DNDBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+
+            string token = "MzQxMDg2NTE0NzM2MDA1MTIw.DF8DKw.ma0rx8Gv6qZJ_tmo5HcpzFhGEoA";
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+            else if (message.Content == "!pong")
+            {
+                await message.Channel.SendMessageAsync("Ping!");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
